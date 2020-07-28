3d25c9fc95e6fec03a1094554ca1fa5e95ee2aea
Sukrit5 <47655182+Sukrit5@users.noreply.github.com>
2020-07-28 13:55:48 -0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,21 +3,15 @@ class Tic_Tac_Toe:
     def __init__(self):
         print("Initialized")
     def drawBoard(self, board):
-        print('   |   |')
-        print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-        print('   |   |')
-        print('-----------')
-        print('   |   |')
-        print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-        print('   |   |')
-        print('-----------')
-        print('   |   |')
-        print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-        print('   |   |')
+       print(board[1] + '|' + board[2] + '|' + board[3])
+       print('---')
+       print(board[4] + '|' + board[5] + '|' + board[6])
+       print('---')
+       print(board[7] + '|' + board[8] + '|' + board[9])
     def inputPlayerLetter(self):
         letter = ''
         while not (letter == 'X' or letter == 'O'):
-            print("Do you want to be X or O?")
+            print("Do you want to be X or O?, Player1")
             letter = input().upper()
         if letter == 'X':
             return ['X', 'O']
@@ -50,18 +44,45 @@ class Tic_Tac_Toe:
             if self.isSpaceFree(board, i):
                 return False
         return True
+    def makeMove(self, board, letter, move):
+       board[move] = letter
+    def getBoardCopy(self, board):
+        boardCopy = []
+        for i in board:
+            boardCopy.append(i)
+        return boardCopy
+    def getPlayerMove(self, board):
+       move = ' '
+       while move not in '1 2 3 4 5 6 7 8 9'.split() or not self.isSpaceFree(board, int(move)):
+           print('What is your next move(1-9')
+           move = input()
+       return int(move)
+       
     def Two_Player_Game(self):
         while True:
             theBoard = [''] * 10
             player1Letter,  player2letter = self.inputPlayerLetter()
             turn = self.whoGoesFirst()
-            print('The' + turn + 'will go  first.')
+            print(turn + ' will go  first.')
             gameIsPlaying = True
             
             while gameIsPlaying:
-                if turn == 'player':
+                if turn == 'player 1':
                     self.drawBoard(theBoard)
-                    move = self.getComputerMove(theBoard, computerLetter)
-                    self.makeMove()
-game = Tic_Tac_Toe()
+                    move = self.getPlayerMove(theBoard)
+                    self.makeMove(theBoard, player1Letter, move)
+                    if self.isWinner(self, theBoard, player1Letter):
+                        print("Player 1 Wins!")
+                        break
+                    turn = 'player 2'
+                elif turn == 'player 2':
+                    self.drawBoard(theBoard)
+                    move = self.getPlayerMove(theBoard)
+                    self.makeMove(theBoard,player2letter , move)
+                    if self.isWinner(self, theBoard, player2letter):
+                        print("Player 2 Wins!")
+                        break
+                    turn = 'player 1'
+                
+game = Tic_Tac_Toe()                   
 game.Two_Player_Game()
